0b9c8780b17936c0f84951da20b2eca2ddc66c03
Camille <camille.caschili-lucas@epitech.eu>
2024-02-25 23:57:12 -0800

    RM old c files

--- C_C++/main.h
+++
@@ -1,3 +0,0 @@
-void get_player_input(int *, int *);
-void display_winner(int);
-float percentage(int, int);
--- C_C++/map.h
+++
@@ -1,5 +0,0 @@
-void print_map(char **);
-int check_map(char **);
-void initialize_map(char **);
-void update_map_and_turn(char **, bool *, int, int, char *);
-void print_colored_text(const char *, int);
